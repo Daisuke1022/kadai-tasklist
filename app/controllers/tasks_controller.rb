749dceac827844d633993e46162cbab2e4173a66
d1022t1994 <mac.d1022xxx@gmail.com>
2018-01-25 07:44:40 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,14 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
-    @task = Task.new(content: 'sample')
+    @task = Task.new
   end
 
   def create
@@ -25,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -40,18 +38,22 @@ class TasksController < ApplicationController
     end
   end
 
-  def destroy
-    @task = Task.find(params[:id])
+　def destroy
+
     @task.destroy
 
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
+　end
+  
+private
+  
+  def set_task
+     @task = Task.find(params[:id])
   end
   
-  private
-  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+  validates :status, presence: true, length: { in: 1..10 }
   validates :content, presence: true, length: { maximum: 255 }
 end
